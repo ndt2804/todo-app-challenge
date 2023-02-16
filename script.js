548e88428b8965e14807d22a43905f9afe74c8d2
noJS <nduytan.uit.a3@gmail.com>
2023-02-16 22:04:16 +0700

    todo-app-challenge

---
+++ script.js
@@ -0,0 +1,49 @@
+const btnAddTask = document.querySelector('button');
+const taskName = document.querySelector('input');
+
+let tasks = getTaskFromLocalStorage()
+renderTask(tasks);
+
+btnAddTask.addEventListener('click', function() {
+    if(!taskName.value) {
+        alert('Nhập công việc')
+        return false
+    }
+    let tasks = getTaskFromLocalStorage()
+    console.log(tasks);
+    tasks.push({nameTask: taskName.value})
+    taskName.value = '';
+
+    localStorage.setItem('tasks', JSON.stringify(tasks))
+    renderTask(tasks);
+
+})
+
+function renderTask(tasks = []) {
+    let listTask = '<ul>';
+   
+    tasks.forEach((task,index) => {
+        listTask += `
+        <li>
+            <div class="task-name">${task.nameTask}</div>
+            <a href="#">Edit</a>
+            <a href="#" onClick="deleteTask(${index})">Delete</a>
+        </li>`
+    });
+    listTask += '</ul>'
+    console.log(listTask);
+
+    document.querySelector('.task-item').innerHTML = listTask;
+
+}
+function getTaskFromLocalStorage() {
+    return localStorage.getItem('tasks') ?  JSON.parse(localStorage.getItem('tasks')) : []
+}
+
+function deleteTask(id) {
+    if(confirm('Bạn có muốn xóa Task không?')) {
+        
+    }
+    
+
+}
